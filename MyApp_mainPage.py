dea0a51d4c352cb5de7b118cabf95810f2f1ae2e
ranzhou <ranzhou@LSBCMB-BHVQWM3.msmyid.uga.edu>
2023-06-16 10:26:55 -0400

    Initial commit

---
+++ MyApp_mainPage.py
@@ -0,0 +1,35 @@
+# Draw a title and some text to the app:
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to TsaiLab's app suite for the 717 genome! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
---
+++ pages/geneID_converter.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import time
+import pandas as pd
+
+st.set_page_config(page_title="GeneID converter Demo 🌱", page_icon="🌱")
+
+idmap='db/h1h2gv5.id.map'
+
+user_suffix_flag = st.radio(
+        "Select how do you want to proceed with suffix if your ID contains 👉",
+        key="suffix",
+        options=["Ignore suffix", "Keep suffix", "I don't know"],
+        )
+
+@st.cache_data
+def load_map(idmap):
+    idmap_dict=dict()
+    with open(idmap) as file:
+        for line in file:
+            lsstr=line.rstrip().split()
+            idmap_dict[lsstr[1]]=lsstr[0]
+            idmap_dict[lsstr[0]]=lsstr[1]
+        return idmap_dict
+
+def user_input_parse(usertxt):
+    usertxt=usertxt.replace("\n"," ")
+    usertxt=usertxt.replace(","," ")
+    x = usertxt.split()
+    return x
+
+def id_convert(parsed_userinput,IDmap):
+    outlist = [];
+    for i in parsed_userinput:
+        try:
+            if "." in i :
+                idbyparts = i.split(".")
+            newid = IDmap[i]
+            outlist.append([i,newid])
+        except:
+            print("An exception occurred")
+    outpd = pd.DataFrame(outlist, columns=['inputID','NewID'])
+    return outpd
+
+user_id_input = st.text_area('You can put the IDs from v4 here: e.g. Ptrev51000006m PoHAPv41023350m')
+IDmap = load_map(idmap)
+
+if st.button('Convert'):
+    converted_id = user_input_parse(user_id_input)
+    output_pddf = id_convert(converted_id,IDmap)
+    st.write('Below is the converted IDs', output_pddf)
